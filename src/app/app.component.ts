94d89ede2eb0e84d7c5b8d355f4aa4cef6c24fd7
hasubasora <330882327@qq.com>
2022-07-13 17:46:47 +0900

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,15 @@ export class AppComponent {
   name = 'Angular ' + VERSION.major;
   title = '4567';
   msg = 'i<em> f u';
-  flag=false;
-  list = ["A","B","C","D"]
+  flag = false;
+  list = ['A', 'B', 'C', 'D'];
+  myclass = 'abc';
+  mystyle = { color: 'green', 'font-size': '18px', 'font-weight': '900' };
+  fnShow(e) {
+    alert('-------!---!---');
+  }
+  current = null;
+  setCurrent(e, i) {
+    this.current = i;
+  }
 }
